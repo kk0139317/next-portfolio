bb621ac0ab65d44ee84b962968b2096d990e772e
kk0139317 <kk4881757@gmail.com>
2024-06-18 12:25:53 +0530

    modified:   pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
 import Layout from './layout'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
@@ -7,10 +7,17 @@ const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+    <title>Your Page Title | Kundan - Freelance Developer</title>
+    <meta name="kundan-portfolio" content=" This is landing page for my portfolio " />
+    {/* Add more meta tags as needed */}
+  </Head>
     <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </QueryClientProvider>
+    </>
   )
 }
