7818831b6a236795e307e49e24caed84ca746c3e
kk0139317 <kk4881757@gmail.com>
2024-06-18 12:33:56 +0530

    modified:   pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,25 @@
-import '../styles/globals.css'
-import Head from 'next/head'
-import Layout from './layout'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import '../styles/globals.css';
+import Head from 'next/head';
+import Layout from './layout';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Head>
-    <title>Your Page Title | Kundan - Freelance Developer</title>
-    <meta name="kundan-portfolio" content=" This is landing page for my portfolio " />
-    {/* Add more meta tags as needed */}
-  </Head>
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </QueryClientProvider>
+      <Head>
+        <title>Kundan - Freelance Developer | Full Stack Developer | Python/Django Developer</title>
+        <meta name="description" content="I am Kundan, a Full Stack Developer specializing in Python/Django. This is my portfolio showcasing my skills and projects." />
+        {/* Additional meta tags for keywords */}
+        <meta name="keywords" content="Freelancer, Django Developer, Python Developer, Web App Developer, Machine Learning Engineer" />
+        <meta name="keywords" content="Software Engineer, Web developer, Chess Player" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
